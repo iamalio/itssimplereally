071b4253adc3206591227ccb58c10a539467425b
iamalio <pzurita.mail@gmail.com>
2022-05-21 00:22:27 -0500

    4 files modified

---
+++ src/components/AboveFold.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class=""></div>
+</template>
+
+<script>
+export default {
+  name: 'AboveFold',
+  
+};
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/MainNav.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class=""></div>
+</template>
+
+<script>
+export default {
+  name: 'MainNav',
+};
+</script>
+
+<style scoped></style>
