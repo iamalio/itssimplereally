bc85374590730feba93cf3289040e24a91209ab5
iamalio <pzurita.mail@gmail.com>
2022-05-21 23:02:52 -0500

    1 file modified

--- src/components/AboveFold.vue
+++ src/components/AboveFold.vue
@@ -1,5 +1,6 @@
 <template>
-
+<section class="container-fluid abovefold">
+</section>
 </template>
 
 <script>
@@ -16,5 +17,7 @@ export default {
 
 
 <style scoped>
-
+.abovefold{
+  background-image: url('https://github.com/iamalio/itssimplereally/blob/master/src/assets/rocketBG.png')
+}
 </style>
